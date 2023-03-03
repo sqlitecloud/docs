812f71ff5b0e22ff2d6690e6c3d7201a0f06e560
TizianoT <tiziano.tuccella@gmail.com>
2023-03-03 10:55:50 +0100

    removed unsed link

--- src/pages/docs/introduction/nav.js
+++ src/pages/docs/introduction/nav.js
@@ -1,26 +1,2 @@
 exports.introduction =
-  [
-    {
-      title: 'Introduction',
-      links: [
-        { title: 'Getting started', href: '/' },
-        { title: 'Installation', href: '/docs/installation' },
-      ],
-    },
-    {
-      title: 'Introduction concepts',
-      links: [
-        { title: 'Understanding caching', href: '/docs/understanding-caching' },
-        {
-          title: 'Predicting user behavior',
-          href: '/docs/predicting-user-behavior',
-        },
-        { title: 'Basics of time-travel', href: '/docs/basics-of-time-travel' },
-        {
-          title: 'Introduction to string theory',
-          href: '/docs/introduction-to-string-theory',
-        },
-        { title: 'The butterfly effect', href: '/docs/the-butterfly-effect' },
-      ],
-    }
-  ]
+  []
--- src/pages/docs/sdk/nav.js
+++ src/pages/docs/sdk/nav.js
@@ -1,26 +1,2 @@
 exports.sdk =
-  [
-    {
-      title: 'SDK',
-      links: [
-        { title: 'Writing plugins', href: '/docs/writing-plugins' },
-        { title: 'Neuralink integration', href: '/docs/neuralink-integration' },
-        { title: 'Temporal paradoxes', href: '/docs/temporal-paradoxes' },
-        { title: 'Testing', href: '/docs/testing' },
-        { title: 'Compile-time caching', href: '/docs/compile-time-caching' },
-        {
-          title: 'Predictive data generation',
-          href: '/docs/predictive-data-generation',
-        },
-      ],
-    },
-    {
-      title: 'SDK details',
-      links: [
-        { title: 'CacheAdvance.predict()', href: '/docs/cacheadvance-predict' },
-        { title: 'CacheAdvance.flush()', href: '/docs/cacheadvance-flush' },
-        { title: 'CacheAdvance.revert()', href: '/docs/cacheadvance-revert' },
-        { title: 'CacheAdvance.regret()', href: '/docs/cacheadvance-regret' },
-      ],
-    }
-  ]
+  []
