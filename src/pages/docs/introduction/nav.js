4adaf748f0468460fd6199155cbf2ab23a8616a3
Marco Bambini <marco@creolabs.com>
2023-03-14 14:45:37 +0100

    Update documentation

--- src/pages/docs/introduction/nav.js
+++ src/pages/docs/introduction/nav.js
@@ -1,2 +1,9 @@
 exports.introduction =
-  []
+  [
+    {title: 'PUB/SUB',
+    links: [
+    { title: 'Implementation Details', href: '/docs/introduction/pubsub_implementation' },
+    { title: 'Payload Format', href: '/docs/introduction/pubsub_payload' },
+    { title: 'Commands', href: '/docs/commands/create-channel' },
+    ],}
+  ]
