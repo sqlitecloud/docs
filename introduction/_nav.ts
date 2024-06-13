b6f93560521164bd265bcf57744e1c1c25ac47b7
Jacob Prall <prall.jacob@gmail.com>
2024-06-13 12:02:53 -0700

    feat: add first three docs

--- introduction/_nav.ts
+++ introduction/_nav.ts
@@ -1,8 +1,8 @@
 import type { SidebarNavStruct } from "@docs-website/types/sidebar-navigation";
 
 const sidebarNav: SidebarNavStruct = [
-	{ title: "Introduction", type: "primary" },
-	{ title: "Dashboard", type: "secondary" },
+	{ title: "Getting Started", type: "primary" },
+	{ title: "Introduction", type: "inner", level: 0 },
 	{ filePath: "introduction/login", type: "inner", level: 0 },
 	{ filePath: "introduction/projects", type: "inner", level: 0 },
 	{ filePath: "introduction/nodes", type: "inner", level: 0 },
