018a0bc8f33d1debc6707b8b9a85c904220dc285
Jacob Prall <prall.jacob@gmail.com>
2024-07-04 12:11:50 -0700

    update python sdk

--- sqlite-cloud/_nav.ts
+++ sqlite-cloud/_nav.ts
@@ -125,6 +125,10 @@ const sidebarNav: SidebarNavStruct = [
 
 	{ title: "JavaScript", type: "inner", level: 0 },
 	{ title: 'Introduction', type: "inner", filePath: "sqlite-cloud/sdks/js/getting-started", level: 1 },
+	{ title: 'React Quick Start', type: "inner", filePath: "sqlite-cloud/quick-start-react", level: 1 },
+
+	{ title: "Python", type: "inner", level: 0 },
+	{ title: 'Introduction', type: "inner", filePath: "sdk-python-introduction", level: 1 },
 
 	{ title: "Go",  type: "inner", level: 0 },
 	{ title: 'Introduction', type: "inner", filePath: "sqlite-cloud/sdks/go/getting-started", level: 1 },
